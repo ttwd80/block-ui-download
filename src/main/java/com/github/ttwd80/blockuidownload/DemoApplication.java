2f90d3d3d548d2ea673c6ce561a1676edf214f3d
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2022-06-09 22:22:29 +1000

    js works

--- src/main/java/com/github/ttwd80/blockuidownload/DemoApplication.java
+++ src/main/java/com/github/ttwd80/blockuidownload/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.github.ttwd80.blockuidownload;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,6 @@ import org.springframework.web.servlet.ModelAndView;
 @SpringBootApplication
 @Controller
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
