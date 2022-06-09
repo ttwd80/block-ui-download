2e6e83aebbcc16334c28deab3824e931f08b1867
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2022-06-09 22:32:15 +1000

    blocked. reproduced

--- src/main/java/com/github/ttwd80/blockuidownload/DemoApplication.java
+++ src/main/java/com/github/ttwd80/blockuidownload/DemoApplication.java
@@ -5,7 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Random;
 
 @SpringBootApplication
 @Controller
@@ -17,6 +23,16 @@ public class DemoApplication {
 	@GetMapping("/")
 	public ModelAndView index() {
 		return new ModelAndView("index");
+	}
+
+	@PostMapping("/do-post")
+	public void doPost(@RequestParam("key") String key, HttpServletResponse response) throws InterruptedException, IOException {
+		long time = 3_000;
+		time += new Random().nextLong(3_000);
+		Thread.sleep(time);
+		response.setContentType("application/x-download");
+		response.setHeader("Content-disposition", "attachment; filename=xoxo.txt");
+		response.getWriter().println("xoxo");
 	}
 
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,3 +1,5 @@
-$(document).ready(function(){
-    
-});
+$(document).ready(function () {});
+
+function block() {
+  $.blockUI({ message: "<h1>Just a moment...</h1>" });
+}
